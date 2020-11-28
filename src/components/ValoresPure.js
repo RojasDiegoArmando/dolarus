ae9fbe6c334aa8d6d6159f82c18a23ca6fa60f7d
Diego Rojas <diegorealmadrid7@gmail.com>
2020-11-28 06:03:46 -0300

    todos los decimales arreglados

--- src/components/ValoresPure.js
+++ src/components/ValoresPure.js
@@ -2,29 +2,33 @@ import React from "react"
 import Typography from "@material-ui/core/Typography"
 
 const CompraPure = ({ cantDolar, cantPeso, dolarBlue, dolarOficial }) => {
-  let dolarSolidario = parseInt(dolarOficial) * 1.65
+  dolarOficial = dolarOficial ? dolarOficial.replace(/,/g, ".") : dolarOficial
+  let dolarSolidario = parseFloat(dolarOficial) * 1.65
+  cantDolar = cantDolar ? cantDolar.replace(/,/g, ".") : cantDolar
+  let comproUsd = parseFloat(cantDolar).toFixed(2)
+  let comproArs = (dolarSolidario * cantDolar).toFixed(2)
   return (
     <div>
       <Typography variant="body1" color="inherit">
         Compraste
       </Typography>
       <Typography variant="body1" color="primary">
-        {cantDolar ? cantDolar : 0} USD
+        {comproUsd} USD
       </Typography>
       <Typography variant="body1" color="inherit">
         por
       </Typography>
       <Typography variant="body1" color="primary">
-        {dolarSolidario * cantDolar} ARS
+        {comproArs} ARS
       </Typography>
     </div>
   )
 }
 
 const VentaPure = ({ cantDolar, cantPeso, dolarOficial }) => {
-  let dolarSolidario = parseInt(dolarOficial) * 1.65
-  let diferencia = cantPeso - dolarSolidario * cantDolar
-
+  dolarOficial = dolarOficial ? dolarOficial.replace(/,/g, ".") : dolarOficial
+  let dolarSolidario = parseFloat(dolarOficial) * 1.65
+  let diferencia = (cantPeso - dolarSolidario * cantDolar).toFixed(2)
   return (
     <div>
       <Typography variant="body1" color="initial">
--- src/components/Value.js
+++ src/components/Value.js
@@ -4,9 +4,9 @@ import { Typography } from "@material-ui/core"
 const ValorBlue = ({ valor, nombre }) => {
   let compra = valor.compra
   let venta = valor.venta
-  compra = (compra ? compra.replace(/,/g,'.') : compra)
-  venta = (venta ? venta.replace(/,/g, '.') : venta)
-  const intermedio = (parseFloat(compra) + parseFloat(venta)) / 2
+  compra = compra ? compra.replace(/,/g, ".") : compra
+  venta = venta ? venta.replace(/,/g, ".") : venta
+  const intermedio = ((parseFloat(compra) + parseFloat(venta)) / 2).toFixed(2)
   return (
     <div>
       <Typography variant="h3" color="inherit" align="center">
@@ -14,7 +14,7 @@ const ValorBlue = ({ valor, nombre }) => {
       </Typography>
       <Typography variant="body1" color="textSecondary" align="center">
         Compra: {compra} <br />
-        Intermedio: {intermedio.toFixed(2)} <br />
+        Intermedio: {intermedio} <br />
         Venta: {venta}
       </Typography>
     </div>
@@ -23,12 +23,13 @@ const ValorBlue = ({ valor, nombre }) => {
 
 const ValorOficial = ({ valor, nombre }) => {
   let compra = valor.compra
-  compra = (compra ? compra.replace(/,/g, '.') : compra)
+  compra = compra ? compra.replace(/,/g, ".") : compra
   let venta = valor.venta
-  venta = (venta ? venta.replace(/,/g, ".") : venta)
+  venta = venta ? venta.replace(/,/g, ".") : venta
   let solidario = venta
-  solidario = (solidario ? solidario.replace(/,/g, '.') : solidario) * 1.65
-  console.log(solidario)
+  solidario = (
+    (solidario ? solidario.replace(/,/g, ".") : solidario) * 1.65
+  ).toFixed(2)
   return (
     <div>
       <Typography variant="h3" color="inherit" align="center">
@@ -37,7 +38,7 @@ const ValorOficial = ({ valor, nombre }) => {
       <Typography variant="body1" color="textSecondary" align="center">
         Compra: {compra} <br />
         Venta: {venta} <br />
-        Solidario: {solidario.toFixed(2)} (65% impuestos)
+        Solidario: {solidario} (65% impuestos)
       </Typography>
     </div>
   )
